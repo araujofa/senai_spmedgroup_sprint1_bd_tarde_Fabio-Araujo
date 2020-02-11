d7bd26e75ad47b1d24b50bb0ad21b41ce825d888
Fabio Araujo <araujosilvafabio@hotmail.com>
2020-02-11 17:52:04 -0300

    Initial commit

---
+++ Scripts/SPMedGroup-01-DDL.sql
@@ -0,0 +1,81 @@
+CREATE DATABASE SP_MedGroup_Tarde
+GO
+Use SP_MedGroup_Tarde
+GO
+CREATE TABLE Enderecos(
+    IdEndereco INT PRIMARY KEY IDENTITY,
+    Estado VARCHAR(255) NOT NULL,
+    Cidade VARCHAR(255) NOT NULL,
+    Bairro VARCHAR(255) NOT NULL,
+    Rua VARCHAR(255) NOT NULL,
+    Numero VARCHAR(255) NOT NULL,
+    CEP CHAR(8) NOT NULL
+);
+
+CREATE TABLE Especialidades(
+    IdEspecialidade INT PRIMARY KEY IDENTITY,
+    Titulo VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE Status(
+    IdStatus INT PRIMARY KEY IDENTITY,
+    Titulo VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Clinicas(
+    IdClinica INT PRIMARY KEY IDENTITY,
+    IdEndereco INT FOREIGN KEY REFERENCES Enderecos (IdEndereco),
+    RazaoSocial VARCHAR(255) NOT NULL,
+    CNPJ CHAR(14) NOT NULL UNIQUE,
+    HrAbertura TIME NOT NULL,
+    HrFechamento TIME NOT NULL
+);
+
+CREATE TABLE NiveisAcesso(
+    IdNivelAcesso INT PRIMARY KEY IDENTITY,
+    Titulo VARCHAR(255) NOT NULL UNIQUE
+);
+
+
+CREATE TABLE Medicos(
+    IdMedico INT PRIMARY KEY IDENTITY,
+    IdEspecialidade INT FOREIGN KEY REFERENCES Especialidades (IdEspecialidade),
+    IdClinica INT FOREIGN KEY REFERENCES Clinicas (IdClinica),
+    IdNivelAcesso INT FOREIGN KEY REFERENCES NiveisAcesso (IdNivelAcesso),
+    Nome VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Senha VARCHAR(255) NOT NULL,
+    Crm VARCHAR(255) NOT NULL UNIQUE
+);
+
+CREATE TABLE Pacientes(
+    IdPaciente INT PRIMARY KEY IDENTITY,
+    IdNivelAcesso INT FOREIGN KEY REFERENCES NiveisAcesso (IdNivelAcesso),
+    IdEndereco INT FOREIGN KEY REFERENCES Enderecos (IdEndereco),
+    Nome VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Senha VARCHAR(255) NOT NULL,
+    DataNacimento DATE NOT NULL,
+    Telefone VARCHAR(255) NOT NULL,
+    RG CHAR(9) UNIQUE,
+    CPF CHAR(11) UNIQUE
+);
+
+CREATE TABLE Administrador(
+    IdAdm INT PRIMARY KEY IDENTITY,
+    IdNivelAcesso INT FOREIGN KEY REFERENCES NiveisAcesso (IdNivelAcesso),
+    Nome VARCHAR(255) NOT NULL,
+    Email VARCHAR(255) NOT NULL UNIQUE,
+    Senha VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE Consultas(
+    IdConsulta INT PRIMARY KEY IDENTITY,
+    IdStatus INT FOREIGN KEY REFERENCES Status (IdStatus),
+    IdMedico INT FOREIGN KEY REFERENCES Medicos (IdMedico),
+    IdPaciente INT FOREIGN KEY REFERENCES Pacientes (IdPaciente),
+    DataConsulta DATE NOT NULL,
+    HoraConsulta TIME,
+    Descricao VARCHAR(255)
+);
+
